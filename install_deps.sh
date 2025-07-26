d060ee37d020b4dad7a058cd0a4c357f77f9d41c
PRINCE <prince@PRINCEs-MacBook-Air.local>
2025-07-26 17:31:26 +0530

    render new

---
+++ install_deps.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+apt-get update && apt-get install -y \
+    cmake \
+    build-essential \
+    libopenblas-dev \
+    liblapack-dev \
+    libx11-dev \
+    libgtk-3-dev \
+    python3-dev
